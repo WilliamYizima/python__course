a5ebbf29bbfa9247be0da4ca7af49778b3677d3d
willima yizima <william.yizima@global.ntt>
2022-04-21 21:57:54 -0300

    Adicionado SHEBANG  e um comentário

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
+
+
+#SHEBANG
+# rode which python no seu terminal para verificar qual é o local que ele está alocado
+# o ambiente virtual
+
+# para rodar o seu programa, é só usar o ./hello.py
+# caso ele de acesso negado, utilize o chmod +x ./hello.py
+# rode novamente , e estará disponível
+
+# O programa está iniciando agora
+
 print('will'.upper())
+
